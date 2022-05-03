748a932f47992058f17cc4f1c253fb862b429643
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2022-05-03 12:57:08 +0200

    Add second header in Tasks, quantity of tasks done and styles.

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -5,5 +5,9 @@ export const Tasks = () => (
   <main className="Tasks">
     <h2 className="Tasks__title">zadania</h2>
     <TaskList/>
+    <h2 className="Tasks__title">skończone</h2>
+    <div className="Tasks__quantity-done-tasks">
+      <span>36</span>
+    </div>
   </main>
 );
