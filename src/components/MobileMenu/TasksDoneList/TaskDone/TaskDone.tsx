3019911d2456a513cd7de39b9c69b4585e014ccb
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2022-05-07 14:41:09 +0200

    Add styles for mobile menu.

--- src/components/MobileMenu/TasksDoneList/TaskDone/TaskDone.tsx
+++ src/components/MobileMenu/TasksDoneList/TaskDone/TaskDone.tsx
@@ -1,1 +1,67 @@
-export const TaskDone = () => <li className="tasks-done-list__task-done"></li>;
+import "./TaskDone.css";
+
+interface Props {
+  id: number;
+  createdAt: string;
+  title: string;
+}
+
+export const TaskDone = (props: Props) => (
+  <li className="TaskDone">
+    <h3 className="TaskDone__title">{props.title}</h3>
+    <span className="TaskDone__date">{props.createdAt}</span>
+    <div className="TaskDone__icon">
+      <svg
+        width="32"
+        height="31"
+        viewBox="0 0 32 31"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g filter="url(#filter0_d_835_493)">
+          <path
+            d="M27.4964 1.34302C28.1661 2.2085 28.1661 3.60711 27.4964 4.47259L13.7821 22.1976C13.1125 23.0631 12.0304 23.0631 11.3607 22.1976L4.50207 13.3351C3.83264 12.4696 3.83264 11.071 4.50207 10.2055C5.17161 9.34005 6.25696 9.34005 6.92661 10.2055L12.5232 17.4963L25.075 1.34302C25.7446 0.476155 26.8268 0.476155 27.4964 1.34302Z"
+            fill="#6D9AB1"
+          />
+        </g>
+        <defs>
+          <filter
+            id="filter0_d_835_493"
+            x="0"
+            y="0.692871"
+            width="31.9987"
+            height="30.1538"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dy="4"/>
+            <feGaussianBlur stdDeviation="2"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_835_493"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow_835_493"
+              result="shape"
+            />
+          </filter>
+        </defs>
+      </svg>
+    </div>
+  </li>
+);
--- src/components/MobileMenu/TasksDoneList/TasksDoneList.tsx
+++ src/components/MobileMenu/TasksDoneList/TasksDoneList.tsx
@@ -1,8 +1,44 @@
-import { TaskDone } from "./TaskDone/TaskDone";
+import {TaskDone} from "./TaskDone/TaskDone";
 import "./TasksDoneList.css";
 
+interface TaskDone {
+  id: number;
+  createdAt: string;
+  title: string;
+}
+
+const fakeTasksDone: TaskDone[] = [
+  {
+    id: 1,
+    createdAt: "20.03.2022, 12:26:17",
+    title: "Dokończyć aplikację To-Do",
+  },
+  {
+    id: 2,
+    createdAt: "20.03.2022, 12:26:17",
+    title: "Wyjść z psem na spacer",
+  },
+  {
+    id: 3,
+    createdAt: "20.03.2022, 12:26:17",
+    title: "Zjeść obiad",
+  },
+  {
+    id: 4,
+    createdAt: "20.03.2022, 12:26:17",
+    title: "Obejrzeć film",
+  },
+  {
+    id: 5,
+    createdAt: "20.03.2022, 12:26:17",
+    title: "Zrobić zakupy",
+  },
+];
+
 export const TasksDoneList = () => (
-  <ul className="tasks-done-list">
-    <TaskDone />
+  <ul className="TasksDoneList">
+    {fakeTasksDone.map((taskdone) => (
+      <TaskDone {...taskdone} key={taskdone.id}/>
+    ))}
   </ul>
 );
