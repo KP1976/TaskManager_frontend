67297a322e25690090db0c99c7af1938635ed623
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2022-06-29 20:07:37 +0200

    Show on main page how many categories are.

--- src/components/Header/HeaderRight/CategoryList/CategoryList.tsx
+++ src/components/Header/HeaderRight/CategoryList/CategoryList.tsx
@@ -1,11 +1,52 @@
-import "./CategoryList.css";
+import { useContext } from "react";
 import { Category } from "./Category/Category";
+import { TasksContext } from "../../../../context/TasksContext";
 
-export const CategoryList = () => (
-  <ul className="CategoryList">
-    <Category categoryNumber={8} categoryName={"rekreacja"} />
-    <Category categoryNumber={11} categoryName={"technologia"} />
-    <Category categoryNumber={10} categoryName={"osobiste"} />
-    <Category categoryNumber={5} categoryName={"jedzenie"} />
-  </ul>
-);
+import "./CategoryList.css";
+
+export const CategoryList = () => {
+  const tasks = useContext(TasksContext);
+  const categories = tasks.map((task) => task.category);
+  const sumOfEachTaskCategory = {
+    recreation: 0,
+    personal: 0,
+    technology: 0,
+    food: 0,
+  };
+
+  categories.forEach((category) => {
+    if (category === "rekreacja") {
+      sumOfEachTaskCategory.recreation += 1;
+    }
+    if (category === "technologia") {
+      sumOfEachTaskCategory.technology += 1;
+    }
+    if (category === "osobiste") {
+      sumOfEachTaskCategory.personal += 1;
+    }
+    if (category === "jedzenie") {
+      sumOfEachTaskCategory.food += 1;
+    }
+  });
+
+  return (
+    <ul className="CategoryList">
+      <Category
+        categoryNumber={sumOfEachTaskCategory.recreation}
+        categoryName={"rekreacja"}
+      />
+      <Category
+        categoryNumber={sumOfEachTaskCategory.technology}
+        categoryName={"technologia"}
+      />
+      <Category
+        categoryNumber={sumOfEachTaskCategory.personal}
+        categoryName={"osobiste"}
+      />
+      <Category
+        categoryNumber={sumOfEachTaskCategory.food}
+        categoryName={"jedzenie"}
+      />
+    </ul>
+  );
+};
