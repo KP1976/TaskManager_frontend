386fb0cd13390ac0c8e0773d2e42f2fe3c0392ed
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2022-07-06 20:40:17 +0200

    Make header responsive for min-width of 414px.

--- src/components/Header/HeaderLeft/LineUnderTitle/LineUnderTitle.tsx
+++ src/components/Header/HeaderLeft/LineUnderTitle/LineUnderTitle.tsx
@@ -1,31 +1,31 @@
 import "./LineUnderTitle.css";
 
 export const LineUnderTitle = () => (
-  <svg
-    className="LineUnderTitle"
-    width="159"
-    height="18"
-    viewBox="0 0 159 18"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M1 17C60.6795 3.39777 130.533 0.684198 158 1.02769"
-      stroke="url(#paint0_linear_709_633)"
-      strokeLinecap="round"
-    />
-    <defs>
-      <linearGradient
-        id="paint0_linear_709_633"
-        x1="158"
-        y1="0.999978"
-        x2="1"
-        y2="17"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop stopColor="white" stopOpacity="0" />
-        <stop offset="1" stopColor="white" />
-      </linearGradient>
-    </defs>
-  </svg>
+  <div className="container">
+    <svg
+      className="LineUnderTitle"
+      viewBox="0 0 159 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 17C60.6795 3.39777 130.533 0.684198 158 1.02769"
+        stroke="url(#paint0_linear_709_633)"
+        strokeLinecap="round"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_709_633"
+          x1="158"
+          y1="0.999978"
+          x2="1"
+          y2="17"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="white" stopOpacity="0" />
+          <stop offset="1" stopColor="white" />
+        </linearGradient>
+      </defs>
+    </svg>
+  </div>
 );
