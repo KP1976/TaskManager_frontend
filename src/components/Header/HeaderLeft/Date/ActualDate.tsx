e1c708b159cb7e6fc9f8de594d18ade1d1a3fa2d
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2022-07-11 18:15:59 +0200

    Add calendar with actual date on desktop resolution.

--- src/components/Header/HeaderLeft/Date/ActualDate.tsx
+++ src/components/Header/HeaderLeft/Date/ActualDate.tsx
@@ -1,6 +1,62 @@
-import "./ActualDate.css";
 import { formatedDate } from "../../../../utils/formatedDate";
 
-export const ActualDate = () => (
-  <span className="ActualDate">{formatedDate(new Date())}</span>
-);
+import "./ActualDate.css";
+
+export const ActualDate = () => {
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth();
+  const day = new Date().getDate();
+  const dayOfTheWeek = new Date().getDay();
+  const daysOfTheWeek = [
+    "poniedziałek",
+    "wtorek",
+    "środa",
+    "czwartek",
+    "piątek",
+    "sobota",
+    "niedziela",
+  ];
+  const months = [
+    "styczeń",
+    "luty",
+    "marzec",
+    "kwiecień",
+    "maj",
+    "czerwiec",
+    "lipiec",
+    "sierpień",
+    "wrzesień",
+    "październik",
+    "listopad",
+    "grudzień",
+  ];
+
+  return (
+    <>
+      <div className="ActualDate-desktop-container">
+        <p className="ActualDate-desktop-container-date">
+          Rok:{" "}
+          <span className="ActualDate-desktop-container-date__year">
+            {year}
+          </span>
+        </p>
+        <p className="ActualDate-desktop-container-date">
+          Miesiąc:{" "}
+          <span className="ActualDate-desktop-container-date__month">
+            {months[month]}
+          </span>
+        </p>
+        <p className="ActualDate-desktop-container-date">
+          Dzień:{" "}
+          <span className="ActualDate-desktop-container-date__day">
+            {day},{" "}
+          </span>
+          <span className="ActualDate-desktop-container-date__day-of-the-week">
+            {daysOfTheWeek[dayOfTheWeek - 1]}
+          </span>
+        </p>
+      </div>
+      <span className="ActualDate">{formatedDate(new Date())}</span>
+    </>
+  );
+};
--- src/components/Header/HeaderLeft/HeaderLeft.tsx
+++ src/components/Header/HeaderLeft/HeaderLeft.tsx
@@ -8,7 +8,7 @@ export const HeaderLeft = () => (
   <div className="HeaderLeft">
     <HamburgerMenu />
     <Title />
-    <div className="calendar">
+    <div className="HeaderLeft__calendar">
       <svg
         focusable="false"
         data-prefix="fas"
@@ -23,7 +23,7 @@ export const HeaderLeft = () => (
           d="M436 160H12c-6.627 0-12-5.373-12-12v-36c0-26.51 21.49-48 48-48h48V12c0-6.627 5.373-12 12-12h40c6.627 0 12 5.373 12 12v52h128V12c0-6.627 5.373-12 12-12h40c6.627 0 12 5.373 12 12v52h48c26.51 0 48 21.49 48 48v36c0 6.627-5.373 12-12 12zM12 192h424c6.627 0 12 5.373 12 12v260c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V204c0-6.627 5.373-12 12-12zm333.296 95.947l-28.169-28.398c-4.667-4.705-12.265-4.736-16.97-.068L194.12 364.665l-45.98-46.352c-4.667-4.705-12.266-4.736-16.971-.068l-28.397 28.17c-4.705 4.667-4.736 12.265-.068 16.97l82.601 83.269c4.667 4.705 12.265 4.736 16.97.068l142.953-141.805c4.705-4.667 4.736-12.265.068-16.97z"
         />
       </svg>
-      <h2 className="calendar__title">kalendarz</h2>
+      <h2 className="HeaderLeft__calendar-title">kalendarz</h2>
     </div>
     <ActualDate />
   </div>
